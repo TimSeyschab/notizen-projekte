47588ec8d0f0c74b93b1ed0f5687d314e8ab0a65
Tim Seyschab <tim@shellnuts.de>
2024-10-13 13:17:42 +0200

    eval infix
    Evalutation of infix operations
    In order to deal with integer and floats we always cast int -> float
    which is not optimal but will do for now

--- go/interpreter/eval/eval.go
+++ go/interpreter/eval/eval.go
@@ -23,6 +23,11 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
 
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
@@ -34,6 +39,93 @@ func Eval(node ast.Node) object.Object {
 	}
 
 	return NULL
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case isNumber(left) && isNumber(right):
+		return evalNumberInfixExpression(operator, left, right)
+	case operator == "==":
+		return getNativeBooleanObject(left == right)
+	case operator == "!=":
+		return getNativeBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func isNumber(obj object.Object) bool {
+	switch obj.Type() {
+	case object.INTEGER_OBJ, object.FLOAT_OBJ:
+		return true
+	default:
+		return false
+	}
+}
+
+func evalNumberInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal, isLeftFloat := getValueAndType(left)
+	rightVal, isRightFloat := getValueAndType(right)
+	if leftVal == nil || rightVal == nil {
+		return NULL
+	}
+
+	switch operator {
+	case "<":
+		return getNativeBooleanObject(leftVal.(float64) < rightVal.(float64))
+	case ">":
+		return getNativeBooleanObject(leftVal.(float64) > rightVal.(float64))
+	case "==":
+		return getNativeBooleanObject(leftVal.(float64) == rightVal.(float64))
+	case "!=":
+		return getNativeBooleanObject(leftVal.(float64) != rightVal.(float64))
+	}
+
+	if isLeftFloat || isRightFloat {
+		return evalFloatInfix(operator, leftVal.(float64), rightVal.(float64))
+	}
+	return evalIntInfix(operator, int64(leftVal.(float64)), int64(rightVal.(float64)))
+}
+
+func getValueAndType(obj object.Object) (interface{}, bool) {
+	switch v := obj.(type) {
+	case *object.Integer:
+		return float64(v.Value), false
+	case *object.Float:
+		return v.Value, true
+	default:
+		return nil, false
+	}
+}
+
+func evalFloatInfix(operator string, leftVal, rightVal float64) object.Object {
+	switch operator {
+	case "+":
+		return &object.Float{Value: leftVal + rightVal}
+	case "-":
+		return &object.Float{Value: leftVal - rightVal}
+	case "*":
+		return &object.Float{Value: leftVal * rightVal}
+	case "/":
+		return &object.Float{Value: leftVal / rightVal}
+	default:
+		return NULL
+	}
+}
+
+func evalIntInfix(operator string, leftVal, rightVal int64) object.Object {
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	default:
+		return NULL
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
--- go/interpreter/eval/eval_test.go
+++ go/interpreter/eval/eval_test.go
@@ -14,6 +14,19 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -26,9 +39,23 @@ func TestEvalFloatExpression(t *testing.T) {
 		input    string
 		expected float64
 	}{
-		{"2.3", 2.3},
+		{"5.3", 5.3},
+		{".3", 0.3},
 		{"0.1", 0.1},
-		{".4", 0.4},
+		{"-5.3", -5.3},
+		{"-.3", -0.3},
+		{"-0.1", -0.1},
+		{"5.0 + 5.2 + 5.3 + 5 - 10.3", 10.2},
+		{"2.2 * 2 * 2.0 * 2 * 2.0", 35.2},
+		{"-50.0 + 100.3 + -50.3", 0},
+		{"5.2 * 2.2 + 10.2", 21.64},
+		{"5.2 + 2.2 * 10.2", 27.64},
+		{"20.0 + 2 * -10.5", -1.0},
+		{"50.0 / 2 * 2 + 10.5", 60.5},
+		{"2.2 * (5 + 10)", 33.0},
+		{"3 * 3 * 3 + 3.5", 30.5},
+		{"3.0 * (3 * 3) + 10.5", 37.5},
+		{"(5.2 + 10 * 2 + 15 / 3) * 2 + -10", 50.4},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -43,6 +70,21 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 == 2", false},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
