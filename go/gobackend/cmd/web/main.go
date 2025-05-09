c0a41af42edda149d379dcf04dd74959cae6171f
Tim Seyschab <tim@shellnuts.de>
2025-05-09 14:19:36 +0200

    Add mongo db connection and docker compose file

--- go/gobackend/cmd/web/main.go
+++ go/gobackend/cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type application struct {
@@ -13,6 +17,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	mongodb_uri := flag.String("dburi", "mongodb://root:example@localhost:27017", "Mongo Database URI")
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
@@ -22,8 +27,22 @@ func main() {
 	}
 
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, app.routes())
+
+	client, err := openDB(*mongodb_uri)
+
+	defer client.Disconnect(context.TODO())
+
+	err = http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(mongodb_uri string) (*mongo.Client, error) {
+	client, err := mongo.Connect(options.Client().ApplyURI(mongodb_uri))
+	if err != nil {
+		return nil, err
+	}
+
+	return client, err
 }
